07765ab51401b5c61bd1c1843c4fdbf3b22d1cc6
Isoide <michalzarzycki23@gmail.com>
2026-03-02 15:04:20 +0100

    Update nl_sgtk.py
    update version

--- nl_sgtk.py
+++ nl_sgtk.py
@@ -13,7 +13,7 @@ from urllib.parse import parse_qs, urlparse
 log = logging.getLogger(__name__)
 
 # Keep a module version to align with setup.py
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 # --------------------------------------------------------------------------------------
 # Field definitions / constants
